696b96d7022a9e2b2a432eb16a89e9d174ba5e1f
jeril g kadavan <jerilgkadavan@gmail.com>
2018-02-21 12:19:19 -0500

    first to github

---
+++ js/main.js
@@ -0,0 +1,101 @@
+"use strict"
+
+var attach = document.getElementById('attach');
+
+function attachmentConsole() {
+    console.log("This is the attachment icon");
+}
+
+attach.addEventListener('click', attachmentConsole)
+
+
+
+var contactI = document.getElementById('contactI');
+
+function contactConsole() {
+    console.log("This is the contact icon");
+}
+
+contactI.addEventListener('click', contactConsole)
+
+
+
+var gridI = document.getElementById('gridI');
+
+function gridConsole() {
+    console.log("This is the grid icon");
+}
+
+gridI.addEventListener('click', gridConsole)
+
+
+
+var handI = document.getElementById('handI');
+
+function handConsole() {
+    console.log("This is the hand icon");
+}
+
+handI.addEventListener('click', handConsole)
+
+
+
+
+var homeI = document.getElementById('homeI');
+
+function homeConsole() {
+    console.log("This is the home icon");
+}
+
+homeI.addEventListener('click', homeConsole)
+
+
+var messageI = document.getElementById('messageI');
+
+function messageConsole() {
+    console.log("This is the message icon");
+}
+
+messageI.addEventListener('click', messageConsole)
+
+
+
+
+var navigationI = document.getElementById('navigationI');
+
+function navigationConsole() {
+    console.log("This is the navigation icon");
+}
+
+navigationI.addEventListener('click', navigationConsole)
+
+
+var reFreshI = document.getElementById('reFreshI');
+
+function reFreshConsole() {
+    console.log("This is the reFresh icon");
+}
+
+reFreshI.addEventListener('click', reFreshConsole)
+
+
+
+var tickBoxI = document.getElementById('tickBoxI');
+
+function tickBoxConsole() {
+    console.log("This is the tick box icon");
+}
+
+tickBoxI.addEventListener('click', tickBoxConsole)
+
+
+var credit_card_icone = document.getElementById('credit_card_icone');
+
+function creditCardConsole() {
+    console.log("This is the credit card icon");
+}
+
+credit_card_icone.addEventListener('click', creditCardConsole)
+
+
+
